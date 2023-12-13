5fa2206aac3abf4d171a42777b36770fc64f5368
jhlee9462 <jhlee9462@epinet.kr>
2023-12-13 13:03:27 +0900

    initial commit

---
+++ src/test/java/com/example/egovsample/EgovSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.egovsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EgovSampleApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
